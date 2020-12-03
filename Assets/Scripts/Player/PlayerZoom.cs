4dcbda270eae4c2f2c12e2b6584478ea2cef8b2a
Piotr_W <46453412+kapitannemo12345@users.noreply.github.com>
2020-12-03 18:57:43 +0100

    Added player zoom script
    :D

---
+++ Assets/Scripts/Player/PlayerZoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerZoom : MonoBehaviour
+{
+
+    public int zoom = 20;
+    public int normal = 60;
+    public float smooth = 5;
+
+
+    private bool isZoomed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            isZoomed = !isZoomed;
+
+        }
+
+        if(isZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+        }
+
+        else
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
+        }
+
+
+
+    }
+}
