81fcf7edd2906666abf42e1237c3d18cd56c7a3b
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2020-12-13 00:09:03 +0100

    Added volume slider - ain't working yet

---
+++ Assets/Scenes/MenuScenes/Volume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeVol(float newValue) {
+        float newVol = AudioListener.volume;
+        newVol = newValue;
+        AudioListener.volume = newVol;
+    }
+}
