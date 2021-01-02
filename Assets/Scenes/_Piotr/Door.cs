53052cfbcdd9f8121cd221f9a6f24108235d0987
Piotr_W <46453412+kapitannemo12345@users.noreply.github.com>
2021-01-02 16:38:28 +0100

    drzwi prawie gotowe warunek otwierania jeszcze zostal

---
+++ Assets/Scenes/_Piotr/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+        animator.SetBool("isOpen", true);
+    }
+
+    public void CloseDoor()
+    {
+        animator.SetBool("isOpen", false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OpenDoor();
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        CloseDoor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
