5d5aa087b07457ea6eefc0c514cb17c21211f15e
Albert Jendrzejczak <aweczet@gmail.com>
2021-03-04 00:43:23 +0100

    refactor: FPSCounter

--- Assets/Scripts/Other/FPSCounter.cs
+++ Assets/Scripts/Other/FPSCounter.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
@@ -12,16 +10,25 @@ public class FPSCounter : MonoBehaviour
     public Text displayText;
     public float refreshRate = 1f;
 
-    private float timer;
+    private float _timer;
 
-    public void Update()
+    private void Start()
     {
-        if(Time.unscaledTime > timer)
-        {
-            float fps = (int)(1f / Time.unscaledDeltaTime);
-            displayText.text = "FPS: " + fps;
-            timer = Time.unscaledTime + refreshRate;
-        }
+        DisplayFPS();
+    }
 
+    private void Update()
+    {
+        if(Time.unscaledTime > _timer)
+        {
+            DisplayFPS();
+        }
+    }
+
+    private void DisplayFPS()
+    {
+        float fps = (int)(1f / Time.unscaledDeltaTime);
+        displayText.text = "FPS: " + fps;
+        _timer = Time.unscaledTime + refreshRate;
     }
 }
