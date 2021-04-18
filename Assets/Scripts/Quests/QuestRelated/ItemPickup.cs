f40514605e58662f0541de304dbe9f5e89ff2c50
granatejro <migos432@gmail.com>
2021-04-18 16:06:20 +0200

    fix: przejscie miedzy prologiem a dniem 1

--- Assets/Scripts/Quests/QuestRelated/ItemPickup.cs
+++ Assets/Scripts/Quests/QuestRelated/ItemPickup.cs
@@ -15,10 +15,11 @@ public class ItemPickup : MonoBehaviour
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+        DontDestroyOnLoad(itemButton);
     }
 
     private void OnDestroy()
-    {
+    {   
         for (int i = 0; i < inventory.slots.Length; i++)
         {
             if (inventory.isFull[i] == false)
