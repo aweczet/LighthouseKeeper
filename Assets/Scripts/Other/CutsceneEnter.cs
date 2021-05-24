42f057a56e452d5365186d7e2683e7d6fdbdcc57
JestemTrzcinskaLaptop <patrycja.trzcinska125@gmail.com>
2021-05-24 13:31:05 +0200

    CutScene mechanism

---
+++ Assets/Scripts/Other/CutsceneEnter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneImageDisplay;
+    public GameObject cutsceneVideoPlayer;
+
+    public int timeDisplay;
+
+
+    void OnTriggerEnter(Collider other) {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutsceneImageDisplay.SetActive(true);
+        cutsceneVideoPlayer.SetActive(true);
+
+        thePlayer.SetActive(false);
+
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut() {
+        yield return new WaitForSeconds(timeDisplay);
+        thePlayer.SetActive(true);
+        cutsceneImageDisplay.SetActive(false);
+        cutsceneVideoPlayer.SetActive(false);
+    }
+}
