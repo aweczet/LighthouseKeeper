6af56ad1c274cbd15682ec327f71179dffa490f3
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-05 15:31:04 +0200

    add: health bar prototype

---
+++ Assets/Scripts/Quests/HarpoonRetrospection/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public int min;
+    public int max;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Color color;
+
+    private void Update()
+    {
+        float currentOffset = current - min;
+        float maxOffset = max - min;
+        float fillAmount = currentOffset / maxOffset;
+        mask.fillAmount = fillAmount;
+        fill.color = color;
+    }
+}
