61a3c7e40f9285aa7530d92a5d27cd30a07e3e50
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-05 13:19:40 +0200

    add: quick cursor toggle

--- Assets/Scripts/SaveSystem/SaveOperator.cs
+++ Assets/Scripts/SaveSystem/SaveOperator.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class SaveOperator : MonoBehaviour
 {
     private Player _player;
+    private bool _cursor;
 
     private void Start()
     {
@@ -14,6 +15,30 @@ public class SaveOperator : MonoBehaviour
     public void DeleteSave()
     {
         SaveSystem.DeleteData();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            ToggleCursor();
+        }
+    }
+
+    private void ToggleCursor()
+    {
+        if (_cursor)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        _cursor = !_cursor;
     }
 
     public void ContinueGame()
