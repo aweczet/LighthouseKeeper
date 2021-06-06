41fbcb73d0034779a976eacabeb2a3fcbbe82a01
granatejro <gm44323@zut.edu.pl>
2021-06-06 15:58:35 +0200

    fix: lighthouse lever

--- Assets/Scenes/_Granat/lighthouse_lever.cs
+++ Assets/Scenes/_Granat/lighthouse_lever.cs
@@ -20,13 +20,13 @@ public class lighthouse_lever : MonoBehaviour
                 if (lever_turned_on == false)
                 {
                     lever_turned_on = true;
-                    lever.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+                    lever.transform.Rotate(-45.0f, 0.0f, 0.0f, Space.Self);
                     lampshade.transform.Translate(0.0f, 3.0f, 0.0f);
                 }
                 else
                 {
                     lever_turned_on = false;
-                    lever.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+                    lever.transform.Rotate(45.0f, 0.0f, 0.0f, Space.Self);
                     lampshade.transform.Translate(0.0f, -3.0f, 0.0f);
                 }
             }
